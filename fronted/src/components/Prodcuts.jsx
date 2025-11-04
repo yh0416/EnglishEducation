393777b81e3b0f288210287e8670f4e4ba45b3bf
yh0416 <yh990416@gmail.com>
2025-11-03 21:09:26 -0500

    Rating component && title organized

--- fronted/src/components/Prodcuts.jsx
+++ fronted/src/components/Prodcuts.jsx
@@ -1,5 +1,6 @@
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
 const Prodcut = ({ product }) => {
   return (
@@ -9,10 +10,16 @@ const Prodcut = ({ product }) => {
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as='div'>
+          <Card.Title as='div' className='product-title '>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+        <Card.Text as='div'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
 
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
